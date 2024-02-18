955c334e4ac02b5fcc9bee2d8d240ebaa7816e05
ananditaputri <ananditaputri1307@gmail.com>
2024-02-18 23:47:10 +0700

    First Commit

---
+++ tugas/tugas1/1a.php
@@ -0,0 +1,5 @@
+<?php
+$_anandita1 = "Topi";
+$_anandita2 = "Bundar";
+print "<h2><i>\"$_anandita1 Saya $_anandita2, $_anandita2 $_anandita1 saya.\"</i></h2>";
+?>
---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,9 @@
+<?php
+$_npm = 18;
+print "Aku adalah angka <b>$_npm</b>";
+print "<br>";
+print "Jika aku dikali 5, maka aku sekarang menjadi <b>" . ($_npm *= 5) . "</b><br>";
+print "Jika aku dibagi 2, maka aku sekarang menjadi <b>" . ($_npm /= 2) . "</b><br>";
+print "Jika aku ditambah 75, maka aku sekarang menjadi <b>" . ($_npm += 75) . "</b><br>";
+print "Jika aku dikurang 20, maka aku sekarang menjadi <b>" . ($_npm -= 20) . "</b>";
+?>
