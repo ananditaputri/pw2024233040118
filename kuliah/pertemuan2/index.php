b17d30e4f0890a4a9f0f0c601e7c06f5a5213d39
ananditaputri <ananditaputri1307@gmail.com>
2024-05-15 12:20:14 +0700

    PHP & MySQL

---
+++ kuliah/pertemuan2/index.php
@@ -0,0 +1,20 @@
+<?php 
+// pertemuan 2 - PHP Dasar
+// sintaks php
+
+// standar output adalah sebuah perintah di php yang digunakan untuk menampilkan sesuatu ke layar mencetak sesuatu ke layar 
+// sintaks echo, print adalah mencetak tulisan, mencetak isi variabel
+// print_r khusus digunakan mencetak isi array
+// var_dump khusus melihat isi variabel
+
+// penulisan sintaks php
+// 1. php di dalam HTML
+// 2. HTML di dalam php
+
+// variabel dan tipe data
+// variabel
+// tidak boleh di awali dengan angka, tapi boleh mengandung angka
+$nama = "anandita";
+
+
+?>
